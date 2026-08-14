ab82259674c885ce4ae1f9103c49dd0e41c2ff1d
Rawad Abou Ghanem <rawadaboughanem0@gmail.com>
2026-08-14 23:23:00 +0300

    updates

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,6 @@
 export const APP_VERSION = "3.0.0";
 export const SERVER_URL =
   process.env.EXPO_PUBLIC_API_URL ?? "https://souqalhlaqyn.duckdns.org";
+export const FALLBACK_SERVER_URL = "http://79.133.56.149:3000";
 
 export const APP_PREFIX = "@barbers-admin";
